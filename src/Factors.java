884fc7c4f5e88015d890b5a75811671ed4a80c3f
Sonny Sasaka <sonnysasaka@gmail.com>
2026-03-28 14:40:33 -0700

    factors use long

--- src/Factors.java
+++ src/Factors.java
@@ -1,9 +1,9 @@
 void main() {
     print("enter a number ");
     Scanner sc = Tools.scanner;
-    int a = sc.nextInt();
-    int b = 1;
-    int c = 0;
+    long a = sc.nextLong();
+    long b = 1;
+    long c = 0;
     while(b <= a) {
         if(a % b == 0) {
             print(b + " ");
